63a46fda96a7448d80527bd8b4de4824fc238978
Jaaq28 <90659536+Jaaq28@users.noreply.github.com>
2021-12-05 23:06:58 -0600

    Registro
    Parte de Registro. autor Jose Adalid Acuna Quiroz

---
+++ Proyecto Final Grupo 3/Entidades/Registro.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Final_Grupo_3.Entidades
+{
+    public class Registro
+    {
+        public int IDENTIDAD { get; set; }
+        public string NOMBRE { get; set; }
+        public string APELIDO { get; set; }
+        public int CELULAR { get; set; }
+        public string DIRECCION { get; set; }
+        public string CORREO { get; set; }
+        public int EDAD { get; set; }
+        public string SEXO { get; set; }
+
+        public Registro(int identidad, string nombre, string apellido, int celular, string direccion, string correo, int edad, string genero)
+        {
+            IDENTIDAD = identidad;
+            NOMBRE = nombre;
+            APELIDO = apellido;
+            CELULAR = celular;
+            DIRECCION = direccion;
+            CORREO = correo;
+            EDAD = edad;
+            SEXO = genero;
+        }
+
+    }
+}
---
+++ Proyecto Final Grupo 3/Vistas/RegistroView.Designer.cs
@@ -0,0 +1,259 @@
+﻿
+namespace Proyecto_Final_Grupo_3.Vistas
+{
+    partial class RegistroView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.IdentidadTextBox = new System.Windows.Forms.TextBox();
+            this.NombreTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ApellidoTextBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.NumCelularTextBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.DireccionTextBox5 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.CorreoTextBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.EdadTextBox = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.GeneroTextBox = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(327, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "REGISTRO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(27, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "No.Identidad:";
+            // 
+            // IdentidadTextBox
+            // 
+            this.IdentidadTextBox.Location = new System.Drawing.Point(144, 109);
+            this.IdentidadTextBox.Name = "IdentidadTextBox";
+            this.IdentidadTextBox.Size = new System.Drawing.Size(209, 22);
+            this.IdentidadTextBox.TabIndex = 2;
+            // 
+            // NombreTextBox
+            // 
+            this.NombreTextBox.Location = new System.Drawing.Point(113, 160);
+            this.NombreTextBox.Name = "NombreTextBox";
+            this.NombreTextBox.Size = new System.Drawing.Size(211, 22);
+            this.NombreTextBox.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(27, 162);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Nombre:";
+            // 
+            // ApellidoTextBox3
+            // 
+            this.ApellidoTextBox3.Location = new System.Drawing.Point(456, 160);
+            this.ApellidoTextBox3.Name = "ApellidoTextBox3";
+            this.ApellidoTextBox3.Size = new System.Drawing.Size(297, 22);
+            this.ApellidoTextBox3.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(375, 160);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Apellido:";
+            // 
+            // NumCelularTextBox4
+            // 
+            this.NumCelularTextBox4.Location = new System.Drawing.Point(135, 203);
+            this.NumCelularTextBox4.Name = "NumCelularTextBox4";
+            this.NumCelularTextBox4.Size = new System.Drawing.Size(131, 22);
+            this.NumCelularTextBox4.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(18, 206);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 20);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "No.Celular:";
+            // 
+            // DireccionTextBox5
+            // 
+            this.DireccionTextBox5.Location = new System.Drawing.Point(415, 205);
+            this.DireccionTextBox5.Name = "DireccionTextBox5";
+            this.DireccionTextBox5.Size = new System.Drawing.Size(377, 22);
+            this.DireccionTextBox5.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(313, 205);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(96, 20);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Direccion:";
+            // 
+            // CorreoTextBox
+            // 
+            this.CorreoTextBox.Location = new System.Drawing.Point(105, 255);
+            this.CorreoTextBox.Name = "CorreoTextBox";
+            this.CorreoTextBox.Size = new System.Drawing.Size(218, 22);
+            this.CorreoTextBox.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(27, 258);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(72, 20);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Correo:";
+            // 
+            // EdadTextBox
+            // 
+            this.EdadTextBox.Location = new System.Drawing.Point(415, 258);
+            this.EdadTextBox.Name = "EdadTextBox";
+            this.EdadTextBox.Size = new System.Drawing.Size(74, 22);
+            this.EdadTextBox.TabIndex = 14;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(352, 258);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(57, 20);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Edad:";
+            // 
+            // GeneroTextBox
+            // 
+            this.GeneroTextBox.Location = new System.Drawing.Point(603, 258);
+            this.GeneroTextBox.Name = "GeneroTextBox";
+            this.GeneroTextBox.Size = new System.Drawing.Size(189, 22);
+            this.GeneroTextBox.TabIndex = 16;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(520, 258);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(76, 20);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Genero:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(66, 365);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(212, 69);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Registrar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // RegistroView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(855, 465);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.GeneroTextBox);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.EdadTextBox);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.CorreoTextBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.DireccionTextBox5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.NumCelularTextBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.ApellidoTextBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.NombreTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.IdentidadTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "RegistroView";
+            this.Text = "RegistroView";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox IdentidadTextBox;
+        private System.Windows.Forms.TextBox NombreTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox ApellidoTextBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox NumCelularTextBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox DireccionTextBox5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox CorreoTextBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox EdadTextBox;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox GeneroTextBox;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button1;
+    }
+}
